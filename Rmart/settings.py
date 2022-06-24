55715ee5d039b867adfaf9913b9a6010da712e1e
Raj Goyanka <rajgoyanka51@gmail.com>
2022-06-24 22:50:58 +0530

    allowed Host

--- Rmart/settings.py
+++ Rmart/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG',default=True,cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['Rmart-Django-Env.eba-x7d5d2kd.us-west-2.elasticbeanstalk.com']
 
 
 # Application definition
