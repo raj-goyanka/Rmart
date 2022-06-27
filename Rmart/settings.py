10f880beb3e9a1640d73c51cd0328830e28b1e41
Raj Goyanka <rajgoyanka51@gmail.com>
2022-06-27 14:37:21 +0530

    Test02

--- Rmart/settings.py
+++ Rmart/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY =os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = ['https://rmart-rajgoyanka.herokuapp.com/','127.0.0.1']
+ALLOWED_HOSTS = ['rmart-rajgoyanka.herokuapp.com','127.0.0.1']
 
 
 # Application definition
